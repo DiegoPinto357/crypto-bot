c803b4d3c6c7859383cec529dcb59aa30a148a98
Diego Pinto <diego.pinto357@gmail.com>
2021-10-07 22:55:29 -0300

    Add socket connection.

--- src/client/App.js
+++ src/client/App.js
@@ -1,8 +1,28 @@
+import React, { useState, useEffect } from 'react';
 import StockChart from './StockChart';
+import socketIOClient from 'socket.io-client';
 import data from '../server/cryptoData/BTC-USDT/1m/aggregated-1632947880000-1633061880000.json';
 
+const ENDPOINT = 'http://127.0.0.1:4001';
+
 const App = () => {
-  return <StockChart data={data} dateTimeFormat="%H:%M" />;
+  const [response, setResponse] = useState('');
+
+  useEffect(() => {
+    const socket = socketIOClient(ENDPOINT);
+    socket.on('FromAPI', data => {
+      setResponse(data);
+    });
+  }, []);
+
+  return (
+    <div>
+      <p>
+        It's <time dateTime={response}>{response}</time>
+      </p>
+      <StockChart data={data} dateTimeFormat="%H:%M" />
+    </div>
+  );
 };
 
 export default App;
--- src/server/index.js
+++ src/server/index.js
@@ -1,18 +1,60 @@
 require('dotenv').config();
-const main = require('./main');
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
 
-const config = {
-  asset: 'BTC',
-  base: 'USDT',
-  timeframe: '1h',
-  loopPeriodRatio: 12,
-  sandboxMode: true,
-  sim: true,
-  simConfig: {
-    dataFile: 'aggregated-1631678400000-1633482000000.json',
-    transientLength: 100,
-    timeMultiplier: 200 * 60,
+const port = 4001;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send({ response: 'I am alive' }).status(200);
+});
+
+const server = http.createServer(app);
+
+const io = socketIo(server, {
+  cors: {
+    origin: '*',
   },
+});
+
+const getApiAndEmit = socket => {
+  const response = new Date();
+  // Emitting a new message. Will be consumed by the client
+  socket.emit('FromAPI', response);
 };
 
-main.run(config);
+let interval;
+
+io.on('connection', socket => {
+  console.log('New client connected');
+  if (interval) {
+    clearInterval(interval);
+  }
+  interval = setInterval(() => getApiAndEmit(socket), 1000);
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+    clearInterval(interval);
+  });
+});
+
+server.listen(port, () => console.log(`Listening on port ${port}`));
+
+// const main = require('./main');
+
+// const config = {
+//   asset: 'BTC',
+//   base: 'USDT',
+//   timeframe: '1h',
+//   loopPeriodRatio: 12,
+//   sandboxMode: true,
+//   sim: true,
+//   simConfig: {
+//     dataFile: 'aggregated-1631678400000-1633482000000.json',
+//     transientLength: 100,
+//     timeMultiplier: 200 * 60,
+//   },
+// };
+
+// main.run(config);
